4a221af9b2416d765394e464ad5f9869a6e28f77
ZARIF ubuntu <uzbzarif@gmail.com>
2021-07-09 21:34:53 +0900

    yolov4

---
+++ YOLO_ONNX/yolov4.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import os
+from PIL import Image
+from matplotlib.pyplot import imshow
+import onnxruntime as rt
+# import colorsys
+import random
+
+def image_preprocess(image, target_size, gt_boxes=None):
+
+    ih, iw = target_size
+    h, w, _ = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_padded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_padded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_padded = image_padded / 255.
+
+    if gt_boxes is None:
+        return image_padded
+
+    else:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_padded, gt_boxes
+
+
+
+# Step 1: Preprocess
+input_size = 416
+
+original_image = cv2.imread("../worker.jpeg")
+original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+original_image_size = original_image.shape[:2]
+
+image_data = image_preprocess(np.copy(original_image), [input_size, input_size])
+image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+print("Preprocessed image shape:",image_data.shape) # shape of the preprocessed input
+
+# Step 3: Inference
+
+sess = rt.InferenceSession("model.onnx")
+
+outputs = sess.get_outputs()
+output_names = list(map(lambda output: output.name, outputs))
+input_name = sess.get_inputs()[0].name
+
+detections = sess.run(output_names, {input_name: image_data})
+print("Output shape:", list(map(lambda detection: detection.shape, detections)))
